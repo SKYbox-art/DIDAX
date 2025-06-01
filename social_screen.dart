d43081e2985e0ea8a4120af71cd6bd527d05d1de
SKYbox-art <dejan.tanasijevic976@gmail.com>
2025-06-01 11:19:25 +0200

    Add files via upload

---
+++ social_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SocialScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Social Module')),
+      body: Center(child: Text('Social content goes here')),
+    );
+  }
+}
